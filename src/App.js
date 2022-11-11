03caf761049964953636f26bb01123c71203f140
nftchance <chance@utc24.io>
2022-11-11 13:46:51 -0600

    feat: cleanup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import { Route, Routes } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async'
-
-import { QueryClient, QueryClientProvider } from 'react-query'
 
 import Meta from './seo/Meta';
 import Navbar from './navbar/Navbar';
@@ -11,27 +8,25 @@ import Application from './applications/Application';
 import Papers from './papers/Papers';
 import Paper from './papers/Paper';
 
-
 import './App.css';
 
-const queryClient = new QueryClient()
-
 function App() {
+	const title = 'cosanostra';
+	const description = 'cosanostra is a community of developers, designers, and entrepreneurs who are passionate about building the future of the internet.';
+
 	return (
-		<HelmetProvider>
-            <Meta title="cosanostra" description="a collective of web3-passionate individuals focusing on research & development of primitives & protocols for on-chain organizations, defi, and everything in-between." />
+		<>
+			<Meta title={title} description={description} />
 
-			<QueryClientProvider client={queryClient}>
-				<Navbar />
+			<Navbar />
 
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="application/" element={<Application />} />
-					<Route path="papers/" element={<Papers />} />
-					<Route path="/papers/:slug" element={<Paper  />} />
-				</Routes>
-			</QueryClientProvider>
-		</HelmetProvider>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="application/" element={<Application />} />
+				<Route path="papers/" element={<Papers />} />
+				<Route path="/papers/:slug" element={<Paper />} />
+			</Routes>
+		</>
 	);
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 
 import { BrowserRouter as Router } from 'react-router-dom';
+
+import { HelmetProvider } from 'react-helmet-async'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+import App from './App';
+
 
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const queryClient = new QueryClient()
+
 root.render(
 	<React.StrictMode>
 		<Router>
-			<App />
+			<HelmetProvider>
+				<QueryClientProvider client={queryClient}>
+					<App />
+				</QueryClientProvider>
+			</HelmetProvider>
 		</Router>
 	</React.StrictMode>
 );
